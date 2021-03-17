98ffed369fc35e876fc9fe490675e65e21a44864
SchindLuder <31135558+SchindLuder@users.noreply.github.com>
2021-03-17 19:19:48 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,6 +5,7 @@ import subprocess
 import numpy
 import threading
 from math import ceil
+import todoist
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
 
 class TodoistSkill(MycroftSkill):
@@ -12,11 +13,16 @@ class TodoistSkill(MycroftSkill):
 		MycroftSkill.__init__(self)
 		
 	def initialize(self):
-		self.token = self.settings.get('Todoist-API-Token')
+		self.api = todoist.TodoistAPI(self.settings.get('Todoist-API-Token'))
 	
 	@intent_handler('shoppinglist.read.intent')
 	def handle_set_temperature(self, message):
 		self.speak_dialog("shoppingList.read")
+		self.api.sync()
+		full_name = api.state['user']['full_name']
+		self.speak_dialog(full_name)		
+		for project in api.state['projects']:
+			self.speak_dialog(str(project['name']))
 		
 			
 def create_skill():
