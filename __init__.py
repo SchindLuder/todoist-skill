4838a0d7d2db676793beeccbf0178f9d57b40b2c
SchindLuder <31135558+SchindLuder@users.noreply.github.com>
2021-03-21 11:43:53 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -9,6 +9,25 @@ import todoist
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
 
 class TodoistSkill(MycroftSkill):
+	def getProjectIdByName(api, name):
+		project = next(x for x in api.state['projects'] if x['name'] == name)
+		return project['id']
+
+	def getOpenItemsOfProject(api, projectName):
+		project_id = getProjectIdByName(api, projectName)
+		return list(filter(lambda x: (x['project_id'] == project_id) & (x['checked'] == 0) , api['items']))
+
+	def getOpenItemsOfProject(api, projectId):
+		return list(filter(lambda x: (x['project_id'] == projectId) & (x['checked'] == 0) , api['items']))
+
+	def addItemToProject(api, projectName, itemName):
+    		project_id = getProjectIdByName(api, projectName)
+    		api.items.add(itemName, project_id=project_id)
+    		api.commit()
+
+	def getContentListFromItems(itemCollection):    
+    		return list(map(lambda x: x['content'], itemCollection))
+	
 	def __init__(self):
 		MycroftSkill.__init__(self)
 		
@@ -33,21 +52,4 @@ class TodoistSkill(MycroftSkill):
 def create_skill():
 	return TodoistSkill()
 
-def getProjectIdByName(api, name):
-    project = next(x for x in api.state['projects'] if x['name'] == name)
-    return project['id']
 
-def getOpenItemsOfProject(api, projectName):
-    project_id = getProjectIdByName(api, projectName)
-    return list(filter(lambda x: (x['project_id'] == project_id) & (x['checked'] == 0) , api['items']))
-
-def getOpenItemsOfProject(api, projectId):
-    return list(filter(lambda x: (x['project_id'] == projectId) & (x['checked'] == 0) , api['items']))
-
-def addItemToProject(api, projectName, itemName):
-    project_id = getProjectIdByName(api, projectName)
-    api.items.add(itemName, project_id=project_id)
-    api.commit()
-
-def getContentListFromItems(itemCollection):    
-    return list(map(lambda x: x['content'], itemCollection))
