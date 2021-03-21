b7f3a4c4977d5c4f02adf8724f4f15782ff5fe49
SchindLuder <31135558+SchindLuder@users.noreply.github.com>
2021-03-21 11:33:47 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,8 +19,30 @@ class TodoistSkill(MycroftSkill):
 	def handle_read_shoppinglist(self, message):
 		self.api.sync()	
 		
-		shoppinglist = self.api.projects.get(2174603341)
-		self.log.info(str(shoppinglist))
+		openItems = getOpenItemsOfProject(self.api, 'Einkaufsliste')
+		itemNames = getContentListFromItems(openItems)
+		
+		for item in getContentListFromItems(openItems):
+			self.speak(item)
 					
 def create_skill():
 	return TodoistSkill()
+
+def getProjectIdByName(api, name):
+    project = next(x for x in api.state['projects'] if x['name'] == name)
+    return project['id']
+
+def getOpenItemsOfProject(api, projectName):
+    project_id = getProjectIdByName(api, projectName)
+    return list(filter(lambda x: (x['project_id'] == project_id) & (x['checked'] == 0) , api['items']))
+
+def getOpenItemsOfProject(api, projectId):
+    return list(filter(lambda x: (x['project_id'] == projectId) & (x['checked'] == 0) , api['items']))
+
+def addItemToProject(api, projectName, itemName):
+    project_id = getProjectIdByName(api, projectName)
+    api.items.add(itemName, project_id=project_id)
+    api.commit()
+
+def getContentListFromItems(itemCollection):    
+    return list(map(lambda x: x['content'], itemCollection))
