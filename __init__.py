27da3a8a102bf34bcbd36072846477f7696a76a1
Matthias Schindler <matthiasschindler92@outlook.de>
2021-09-26 22:33:53 +0200

    optional parameters

--- __init__.py
+++ __init__.py
@@ -54,7 +54,7 @@ class TodoistSkill(MycroftSkill):
 			self.speak('ich hab den gewünschten Eintrag nicht verstanden')
 			return
 		
-		self.todoist.addItemToProject('Einkaufsliste', str(listItem), True)
+		self.todoist.addItemToProject('Einkaufsliste', str(listItem), None, True)
 		
 		self.speak_dialog('project.added.item', {
 			'project': 'Einkaufsliste', 
