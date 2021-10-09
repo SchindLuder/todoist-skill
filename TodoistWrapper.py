26df56f15eb45f94b280e3302e51c63b37c96138
Matthias Schindler <matthiasschindler92@outlook.de>
2021-10-09 12:56:00 +0200

    several items of one matching group need to be ordered

--- TodoistWrapper.py
+++ TodoistWrapper.py
@@ -94,11 +94,7 @@ class TodoistWrapper():
 
 		def removeIgnoredItems(item):			
 			return (item['section_id'] is not ignoreSection) or ('http' not in item)
-			
-		shoppingItems = list(filter(removeIgnoredItems, shoppingItems))
-
-
-		#regex = r'[0-9½¼¾\-]{1,5}[ kgeh\.ml]{0,9}((\bEL\b)|(\bTL\b)|(\bStängel\b)|(\bZweige\b)|(\bStück\b)|(\bLiter\b)|(\bPackung\b)|(\bBund\b)|(\bPack\b)|(\bPäckchen\b)|(\bPk\b)|(\bFlasche\b)|(\bPrise\b)|(\bPrisen\b)){0,1}(.*zum (Braten|Kochen|Würzen){1})$'
+					
 		regex = r'[0-9½¼¾\-]{0,5}\s{0,1}(g |kg |ml |l |geh\. |gestrichener |gestr\. ){0,1}(grüne |rote |frische |gemischte |reife |getrocknete |gefrorene ){0,}((\bEL\b)|(\bTL\b)|(\bStängel\b)|(\bZweige\b)|(\bStück\b)|(\bLiter\b)|(\bPackung\b)|(\bBund\b)|(\bPack\b)|(\bPäckchen\b)|(\bPk\b)|(\bFlasche\b)|(\bPrise\b)|(\bPrisen\b)){0,1}\s{0,1}(?P<ingredient>[\D\-]{,})'
 
 		for shoppingItem in shoppingItems:			
@@ -114,16 +110,11 @@ class TodoistWrapper():
 						
 			match = re.search(regex, name)
 			
-			if match is not None: 
-				#full name as in recipe / shopping list
-				#previousName = name + rest
-				# replace amount and leading/trailing whitespaces
-				#name = re.sub(regex, '', name).strip()						
+			if match is not None: 				
 				name = match.group('ingredient')
-				#self.log(str(previousName) + ' was converted to ' + str(name))				
-				#if previousName not in itemsWithAmounts:
+
 				if fullName not in itemsWithAmounts:
-					itemsWithAmounts[name] = fullName    		
+					itemsWithAmounts[fullName] = name    		
 			
 			if name in itemOrderIds: 
 				sortedItems[itemOrderIds[name]] = name
@@ -158,30 +149,42 @@ class TodoistWrapper():
 		for listItem in list(filter(lambda x: x is not None, sortedItems)):
 			childOrders[childOrderCount] = listItem
 			childOrderCount += 1
-			
-		#reorder items
-		for childOrder in childOrders:
-			name = childOrders[childOrder]
-			
-			matchingItem = next((x for x in shoppingItems if x['content'] == name),None)
-			if matchingItem is None:
-				if not name in itemsWithAmounts:
-					continue
-				
-				#get original entry with amount...
-				previousName = itemsWithAmounts[name]
-
-				try:
-					matchingItem = next(x for x in shoppingItems if x['content'] == previousName)
-
-				except:
-					self.log(f'could not find matching item for {previousName}')
-					continue
-				
-			matchingItem.reorder(child_order = childOrder)
 		
-		self.log('commiting changes')
+		offset = 1
+		
+		sorted_keys = sorted(childOrders.keys())
+
+		for orderNumber in sorted_keys:			
+			orderName = str(childOrders[orderNumber])		
+
+			def doesItemBelongToType(shoppingListItem):
+				itemName = str(shoppingListItem['content'])
+				if itemName == orderName:
+					return True
+
+				if itemName not in itemsWithAmounts:
+					return False
+
+				if orderName == itemsWithAmounts[itemName]:
+					return True
+
+				return False
+
+			itemsOfThisType = list(filter(doesItemBelongToType, shoppingItems))
+
+			for itemOfThisType in itemsOfThisType:				
+				itemOfThisType.reorder(child_order = int(offset))				
+				self.log(str(offset) + ' : ' + itemOfThisType['content'])
+
+				if offset % 10 == 0:
+					self.log('commiting 10 changes')
+					self.api.commit();
+
+				offset += 1
+
+		self.log('commiting remaining changes')
 		self.api.commit();
+
 
 	def getOrAddSection(self, projectName, sectionName):
 		projectId = self.getProjectIdByName(projectName)
